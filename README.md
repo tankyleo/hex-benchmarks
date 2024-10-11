325d591afd0d0ead9a2ac7dd8aa4dc672866c1cf
Leo Nash <hello@leonash.net>
2024-10-11 07:13:36 +0000

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+```
+sudo apt install valgrind
+cargo install --version 0.14.0 iai-callgrind-runner
+cargo bench
+```
---
+++ benches/library_benchmark.rs
@@ -0,0 +1,61 @@
+use iai_callgrind::{library_benchmark, library_benchmark_group, main};
+use std::fmt::Write;
+use std::hint::black_box;
+use std::vec::IntoIter;
+
+use hex::BytesToHexIter;
+use hex::DisplayHex;
+
+fn display_hex(mut v: (String, Vec<u8>)) {
+    write!(v.0, "{}", v.1.as_hex()).unwrap();
+}
+
+fn iter(mut v: (String, BytesToHexIter<IntoIter<u8>>)) {
+    for char in v.1 {
+        v.0.push(char);
+    }
+}
+
+#[library_benchmark]
+#[bench::sevenmib(args = (7 * 1024 * 1024), setup = setup_display_hex)]
+#[bench::sevenkib(args = (7 * 1024), setup = setup_display_hex)]
+#[bench::onekib(args = (1024), setup = setup_display_hex)]
+#[bench::small(args = (128), setup = setup_display_hex)]
+fn bench_display_hex(v: (String, Vec<u8>)) {
+    black_box(display_hex(v))
+}
+
+#[library_benchmark]
+#[bench::sevenmib(args = (7 * 1024 * 1024), setup = setup_iter)]
+#[bench::sevenkib(args = (7 * 1024), setup = setup_iter)]
+#[bench::onekib(args = (1024), setup = setup_iter)]
+#[bench::small(args = (128), setup = setup_iter)]
+fn bench_iter(v: (String, BytesToHexIter<IntoIter<u8>>)) {
+    black_box(iter(v))
+}
+
+library_benchmark_group!(
+name = bench_encoding_group;
+    benchmarks = bench_display_hex, bench_iter
+    );
+
+main!(library_benchmark_groups = bench_encoding_group);
+
+fn setup_display_hex(size: usize) -> (String, Vec<u8>) {
+    use rand::RngCore;
+    let mut src = vec![0u8; size];
+    let mut rng = rand::thread_rng();
+    rng.fill_bytes(&mut src);
+    let dest = String::with_capacity(2 * size);
+    (dest, src)
+}
+
+fn setup_iter(size: usize) -> (String, BytesToHexIter<IntoIter<u8>>) {
+    use rand::RngCore;
+    let mut src = vec![0u8; size];
+    let mut rng = rand::thread_rng();
+    rng.fill_bytes(&mut src);
+    let dest = String::with_capacity(2 * size);
+    //(dest, BytesToHexIter::new(src.into_iter()))
+    (dest, BytesToHexIter::new(src.into_iter(), hex::Case::Lower))
+}
