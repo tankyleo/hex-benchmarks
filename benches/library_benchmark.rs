9da0c0958b824798014f7ced3f743175e781e7d8
Leo Nash <hello@leonash.net>
2024-10-13 03:06:35 +0000

    Add `BufEncoder` benchmarks

--- benches/library_benchmark.rs
+++ benches/library_benchmark.rs
@@ -5,6 +5,7 @@ use std::vec::IntoIter;
 
 use hex::BytesToHexIter;
 use hex::DisplayHex;
+use hex::buf_encoder::BufEncoder;
 
 fn display_hex(mut v: (String, Vec<u8>)) {
     write!(v.0, "{}", v.1.as_hex()).unwrap();
@@ -14,6 +15,10 @@ fn iter(mut v: (String, BytesToHexIter<IntoIter<u8>>)) {
     for char in v.1 {
         v.0.push(char);
     }
+}
+
+fn buf_encoder<const CAP: usize>(mut v: (BufEncoder<CAP>, Vec<u8>)) {
+    v.0.put_bytes(v.1);
 }
 
 #[library_benchmark]
@@ -34,9 +39,28 @@ fn bench_iter(v: (String, BytesToHexIter<IntoIter<u8>>)) {
     black_box(iter(v))
 }
 
+#[library_benchmark]
+#[bench::onekib(setup = setup_buf_encoder::<1024>)]
+fn bench_onekib_buf_encoder(v: (BufEncoder<1024>, Vec<u8>)) {
+    black_box(buf_encoder(v))
+}
+
+#[library_benchmark]
+#[bench::sevenkib(setup = setup_buf_encoder::<7168>)]
+fn bench_sevenkib_buf_encoder(v: (BufEncoder<7168>, Vec<u8>)) {
+    black_box(buf_encoder(v))
+}
+
+#[library_benchmark]
+#[bench::onemib(setup = setup_buf_encoder::<1048576>)]
+fn bench_onemib_buf_encoder(v: (BufEncoder<1048576>, Vec<u8>)) {
+    black_box(buf_encoder(v))
+}
+
+
 library_benchmark_group!(
 name = bench_encoding_group;
-    benchmarks = bench_display_hex, bench_iter
+    benchmarks = bench_display_hex, bench_iter, bench_onekib_buf_encoder, bench_sevenkib_buf_encoder, bench_onemib_buf_encoder,
     );
 
 main!(library_benchmark_groups = bench_encoding_group);
@@ -59,3 +83,13 @@ fn setup_iter(size: usize) -> (String, BytesToHexIter<IntoIter<u8>>) {
     //(dest, BytesToHexIter::new(src.into_iter()))
     (dest, BytesToHexIter::new(src.into_iter(), hex::Case::Lower))
 }
+
+fn setup_buf_encoder<const CAP: usize>() -> (BufEncoder<CAP>, Vec<u8>) {
+    use rand::RngCore;
+    let mut src = vec![0u8; CAP / 2];
+    let mut rng = rand::thread_rng();
+    rng.fill_bytes(&mut src);
+    let dest = BufEncoder::new(hex::Case::Lower);
+    (dest, src)
+}
+
